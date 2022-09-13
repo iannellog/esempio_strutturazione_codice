30d59b884b4fec594d968175de0d8d0f52a95ec5
Giulio Iannello <g.iannello@unicampus.it>
2022-09-13 11:19:06 +0200

    Introduzione di sottoprogrammi per decomporre la soluzione in parti ciascuna delle quali ha un compito ben definito. In particolare, viene definita un’interfaccia per la funzione che acquisisce un record e che resterà invariata in tutte le soluzioni successive.

--- code/load_agenda.py
+++ code/load_agenda.py
@@ -6,33 +6,91 @@ Created on April 25, 2022
 @author: Giulio Iannello
 
 acquisizione di record da inserire in un'agenda
-versione base in cui il numero di record da acquisire non è
-fornito a priori
+versione base strutturata con sottoprogrammi che definiscono un'interfaccia
+per la funzione "get_record"
 """
 
 from json import dump
 
-# acquisisce i record
-agenda = []
-while True:
+
+def get_record():
+    """
+    acquisisce un record e lo restituisce sotto forma di dizionario
+    restituisce un dizionario vuoto per indicare che non vi sono altri
+    record da acquisire
+
+    Returns
+    -------
+    dict
+        il record sotto forma di dizionario
+        restituisce un dizionario vuoto per indicare che non vi sono altri
+        record da acquisire
+    """
     nome = input('Nome: ')
-    if nome == 'stop':  # non vi sono altri record da acquisire
-        break
+    if nome == 'stop':
+        return {}
     record = {}
     record['nome'] = nome
     record['cognome'] = input('Cognome: ')
     record['telefono'] = input('Telefono: ')
     record['email'] = input('Email: ')
-    agenda.append(record)
+    return record
 
-# visualizza i record acquisiti
-for record in agenda:
+
+def display_records(agenda):
+    """
+    visualizza i record in agenda
+
+    Parameters
+    ----------
+    agenda : list
+        lista di record da visualizzare
+        ogni record è un dizionario
+
+    Returns
+    -------
+    None.
+
+    """
+    for record in agenda:
+        print('-----------')
+        for key in record:
+            print(f'{key}: {record[key]}')
     print('-----------')
-    for key in record:
-        print(f'{key}: {record[key]}')
-print('-----------')
 
-# salva i record acquisiti in formato json
-fout = open('../temp/agenda.json', 'w')
-dump(agenda, fout, indent=3)
-fout.close()
+
+def save_json(obj, fname, indnt=3):
+    """
+    salva un oggetto python (lista o dizionario) in formato json
+
+    Parameters
+    ----------
+    obj : list o dict
+        oggetto python da salvare in formato json
+    fname : str
+        path del file in cui salvare obj
+    indent : int, optional
+        indentazione da usare per il formato json; il default è 3.
+
+    Returns
+    -------
+    None.
+
+    """
+    fout = open(fname, 'w')
+    dump(obj, fout, indent=indnt)
+    fout.close()
+
+
+if __name__ == "__main__":
+    # acquisisce i record
+    agenda = []
+    while True:
+        record = get_record()
+        if record == {}:  # non vi sono altri record da acquisire
+            break
+        agenda.append(record)
+
+    display_records(agenda)
+
+    save_json(agenda, '../temp/agenda.json')
